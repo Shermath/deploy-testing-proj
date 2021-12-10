7b2e0ffb596e9a9c9ae283e95ed72a13dd52e6f8
Sherin Manalel <sherinmanalel@Sherins-MacBook-Pro.local>
2021-12-10 14:23:36 +1100

    Delete some code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>My Sample React App</h1>
+      <h1>This is my Sample react project.</h1>
       <h2>Sherin Mathew</h2>
       <h3>Manalel</h3>
       <img src="https://i.guim.co.uk/img/media/0fb2e0045ccbb19b83c84b3049e4cec8e9714192/0_149_4245_2547/master/4245.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=1151659b78090856e741e874e4cc0541" alt="Image Nature" />
