b179cfc5a82d70e739bc10dfd8a6f1e79c11c646
Sherin Manalel <sherinmanalel@Sherins-MacBook-Pro.local>
2021-12-10 14:06:21 +1100

    Manalel added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>My Sample React App</h1>
+      <h2>Sherin Mathew</h2>
+      <h3>Manalel</h3>
+      <img src="https://i.guim.co.uk/img/media/0fb2e0045ccbb19b83c84b3049e4cec8e9714192/0_149_4245_2547/master/4245.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=1151659b78090856e741e874e4cc0541" alt="Image Nature" />
     </div>
   );
 }
